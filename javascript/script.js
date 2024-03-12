9e2e919f351da1f765b1973b57d97388bc2be0a4
Talles <81382968+Talleshts@users.noreply.github.com>
2024-03-12 19:18:14 -0300

    ajuste

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,15 @@
 var teste = "Teste"
-let soma = numeroA + numeroB
-const testeConstante = [1, 2, 3, 4]
+const testeConstante = [1, 2, 3, 4, "Cachorro"]
 const numeroA = 10
 const numeroB = 20
 const numeroFake = "5"
+let soma = numeroA + numeroB
 
 console.log("SOMA FAKE: " + numeroA + numeroFake);
 console.log("SUBTRAÇÂO FAKE: " + numeroA - numeroFake);
 console.log("DIVISÃO FAKE: " + numeroB - numeroFake);
 console.log("MULTIPLICAÇÃO FAKE: " + numeroA * numeroFake);
+log
 
 document.write("URRRRRRRRRRRRRRRRRUUUUUUUUL")
 alert("Eai")
