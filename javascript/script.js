2f09e60a8a6bc4de3e7ce6dc4d255aa807d7f1b6
Talles <81382968+Talleshts@users.noreply.github.com>
2024-03-12 19:07:29 -0300

    Operações fakes

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,14 @@
+var teste = "Teste"
+let soma = numeroA + numeroB
+const testeConstante = [1, 2, 3, 4]
+const numeroA = 10
+const numeroB = 20
+const numeroFake = "5"
+
+console.log("SOMA FAKE: " + numeroA + numeroFake);
+console.log("SUBTRAÇÂO FAKE: " + numeroA - numeroFake);
+console.log("DIVISÃO FAKE: " + numeroB - numeroFake);
+console.log("MULTIPLICAÇÃO FAKE: " + numeroA * numeroFake);
+
 document.write("URRRRRRRRRRRRRRRRRUUUUUUUUL")
 alert("Eai")
